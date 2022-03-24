68e07882b84704c52353bdfef89f9d1cb4d82240
IanoSang <ianosang18@gmail.com>
2022-03-24 21:31:09 +0300

    replacxes the buttons with an funtional add to cart button

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,26 @@ $(".submit").click(function() {
     alert("item added to cart");
 
 });
-var addItemId = 0;
 
-function addToCart(item) {
+var addItemId = 0;
+function addToCart (item) {
     addItemId += 1;
     var selectedItem = document.createElement('div');
     selectedItem.classList.add('cartImg');
-    selectedItem.setAttribute('id', addItemId);
+    selectedItem.setAttribute('id',addItemId);
     var img = document.createElement('img');
-    img.setAttribute('src', item.children[0].currentSrc);
+    img.setAttribute('src',item.children[0].currentSrc);
+    var title = document.createElement('div')
+    title.innerText = item.children[1].innerText;
+    var delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+   delBtn.setAttribute('onclick','del('+addItemId+')');
     var cartItems = document.getElementById('title');
     selectedItem.append(img);
+    selectedItem.append(title);
+    selectedItem.append(delBtn);
     cartItems.append(selectedItem);
+}
+function del(item){
+    document.getElementById(item).remove();
 }
